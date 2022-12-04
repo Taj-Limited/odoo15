659e2553b4f0edcaabc18a32b187b43fb3b30ce3
mmbashiti <mmbashiti@gmail.com>
2022-12-04 14:10:41 +0200

    Update account_move.py

--- move_invoice_line/models/account_move.py
+++ move_invoice_line/models/account_move.py
@@ -2,6 +2,7 @@ from odoo import api, models
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
+    
     vehicle_id = fields.Many2one('fleet.vehicle', string='Vehicle', index=True)
     order_id = fields.Many2one('sale.order', string='Sale Order', index=True)
     route_id = fields.Many2one('sale.order.line', string='Route', index=True)
