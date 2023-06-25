d36d77f64ed6c63b94cd8641a36805fcb8b5a2e3
Moaiad <mmbashiti@gmail.com>
2023-06-25 13:24:24 +0300

    add customer vrn

--- move_invoice_line/models/inherit_partner.py
+++ move_invoice_line/models/inherit_partner.py
@@ -4,4 +4,4 @@ from odoo import models, api, fields
 class InheritPartner(models.Model):
     _inherit = 'res.partner'
 
-    vrn = fields.Text(string='VRN NO',  store=True, readonly=True)
+    vrn = fields.Char(string='VRN NO',  store=True, require=False)
