f893be46d09fdfa93e515973b01c91f6c92e9968
madfoxuser5@gmail.com <LetsCodeFun@2024#:)>
2024-11-27 16:11:29 +0300

    Fixes move line

--- move_invoice_line/models/inherit_purchase_order.py
+++ move_invoice_line/models/inherit_purchase_order.py
@@ -6,20 +6,20 @@ class PurchaseOrderInherit(models.Model):
 
     sale_order_id = fields.Many2one('sale.order', 'Sale Order')
 
-    # def action_create_invoice(self):
-    #     action = super(PurchaseOrderInherit, self).action_create_invoice()
-    #     # account_move_id = self.env['account.move'].sudo().search([("purchase_id", "=", self.id)])
-    #     # print("account_move_id", account_move_id)
-    #     # user_account_type = self.env['account.account.type'].sudo().search([('type', '=', 'payable')])
-    #     for rec in self.sale_order_id.order_line:
-    #         for rec_line in self.invoice_ids[0].line_ids:
-    #             if rec_line.account_id.account_type != 'liability_payable':
-    #                 rec_line.container_num = rec.container_num
-    #                 rec_line.file_name = rec.file_name
-    #                 rec_line.consignee = rec.consignee
-    #                 rec_line.weight = rec.weight
-    #                 rec_line.size = rec.size
-    #                 rec_line.srn = rec.srn
-    #                 rec_line.vehicle_id = rec.vehicle_id.id
-    #                 rec_line.order_id = rec.order_id.id
-    #                 rec_line.route_id = rec.id
+    def action_create_invoice(self):
+        action = super(PurchaseOrderInherit, self).action_create_invoice()
+        # account_move_id = self.env['account.move'].sudo().search([("purchase_id", "=", self.id)])
+        # print("account_move_id", account_move_id)
+        # user_account_type = self.env['account.account.type'].sudo().search([('type', '=', 'payable')])
+        for rec in self.sale_order_id.order_line:
+            for rec_line in self.invoice_ids[0].line_ids:
+                if rec_line.account_id.account_type != 'liability_payable':
+                    rec_line.container_num = rec.container_num
+                    rec_line.file_name = rec.file_name
+                    rec_line.consignee = rec.consignee
+                    rec_line.weight = rec.weight
+                    rec_line.size = rec.size
+                    rec_line.srn = rec.srn
+                    rec_line.vehicle_id = rec.vehicle_id.id
+                    rec_line.order_id = rec.order_id.id
+                    rec_line.route_id = rec.id
